a87140edaa25affcd719100ad5f4aabff7734034
Daniel Glez de Chaves González <72324395+DanielGlezDeChavesGonzalez@users.noreply.github.com>
2024-02-19 18:12:00 +0000

    pipeline

--- ejemplo_cli.py
+++ ejemplo_cli.py
@@ -2,64 +2,100 @@ import click
 from loguru import logger
 import math
 from typing import Any, Callable
+import os
+import numpy as np
+import pandas as pd
 
-def setup_logger(log_to_file: bool, log_level: str) -> None:
-    """
-    Configure the logger based on user preferences.
+def read_folder(folder_to_read: str) -> Any:
 
-    Args:
-    log_to_file (bool): If True, logs are written to a file. If False, logs are printed to the console.
-    log_level (str): Level of logging. Example: 'INFO', 'ERROR'.
+    data_array = []
 
-    Returns:
-    None
-    """
-    if log_to_file:
-        logger.add("debug.log", format="{time} {level} {message}", level=log_level.upper())
-    else:
-        logger.remove()
-        logger.add(lambda msg: click.echo(msg, err=True), format="{time} {level} {message}", level=log_level.upper())
+    for filename in os.listdir(folder_to_read):
+        file_path = os.path.join(folder_to_read, filename)
+    
+        if os.path.isfile(file_path):
+            data = pd.read_csv(file_path, sep=';', header=None)
+        else:
+            print(f"'{file_path}' does not exist or is not a file.")
+        data_array.append(data)
+        
+    data.info()
+    return data_array
 
-def calculate_factorial(number: int) -> int:
-    """
-    Calculate the factorial of a given number.
+def cleaning(data_array : Any ) -> Any:
+        
+    ## Remove duplicates for all files
+    data_array = [data.drop_duplicates() for data in data_array]
+        
+    ## Remove empty strings
+    data_array = [data.replace(r'^\s*$', np.nan, regex=True) for data in data_array]
 
-    Args:
-    number (int): A non-negative integer.
+    return data_array
 
-    Returns:
-    int: Factorial of the given number.
-    """
-    if number < 0:
-        logger.error(f"Factorial of a negative number {number} is not defined.")
-        raise ValueError("Factorial of a negative number is not defined.")
-    return math.factorial(number)
+def transformations(data_array: Any) -> Any:
+    
+    # Fill in the missing values with the mean of the 3 previous values and 3 following values
+    data_array = [data.fillna(data.rolling(window=7, min_periods=1).mean()) for data in data_array]
+    
+    return data_array
+
+def normalize(data_array : Any) -> Any:
+    
+    # Normalize the data
+    data_array = [data.apply(lambda x: (x - np.min(x)) / (np.max(x) - np.min(x))) for data in data_array]
+    
+    return data_array
+
+def unnormalize(data_array : Any) -> Any:
+        
+    # Unnormalize the data
+    data_array = [data.apply(lambda x: (x * (np.max(x) - np.min(x))) + np.min(x)) for data in data_array]
+        
+    return data_array
+
+def aumented_data(data_array: Any) -> Any:
+        
+    # Aumented data
+    data_array = [data.append(data.rolling(window=7, min_periods=1).mean()) for data in data_array]
+        
+    return data_array
+    
+def write_file(data_array: Any, folder_to_write: str) -> None:
+    
+    if not os.path.exists(folder_to_write):
+        os.makedirs(folder_to_write)
+        
+    for i in range(len(data_array)):
+        data_array[i].to_csv(folder_to_write + "/file_" + str(i) + ".csv", sep=';', index=False)
+        
+    return None
+
 
 @click.command()
-@click.argument('number', type=int)
-@click.option('--log-to-file', is_flag=True, help="Enable logging to a file instead of the console.")
-@click.option('--log-level', default='INFO', type=str, help="Set the logging level (e.g., INFO, ERROR).")
-def main(number: int, log_to_file: bool, log_level: str) -> None:
-    """
-    CLI tool to calculate and print the factorial of a given number.
+@click.argument('folder_to_read', type=str)
+@click.argument('folder_to_write', type=str)
+# @click.option('--log-to-file', is_flag=True, help="Enable logging to a file instead of the console.")
+def main(folder_to_read: str, folder_to_write: str) -> None:
 
-    Args:
-    number (int): A non-negative integer for which factorial is to be calculated.
+    data = read_folder(folder_to_read)
+    
+    print ("archivos: " + str(len(data)))
+        
+    clean_data = cleaning(data)
+        
+    transformations_data = transformations(clean_data)
+    
+    write_file(transformations_data, folder_to_write + "/transformed")
+    
+    normalize_data = normalize(transformations_data)
+    
+    write_file(normalize_data, folder_to_write + "/normalized")
 
-    Options:
-    --log-to-file: If set, logs will be written to a file.
-    --log-level: The logging level, such as INFO or ERROR.
-
-    Returns:
-    None
-    """
-    setup_logger(log_to_file, log_level)
-
-    try:
-        result = calculate_factorial(number)
-        click.echo(f"El factorial de {number} es {result}")
-    except ValueError as e:
-        click.echo(e)
+    unnormalize_data = unnormalize(normalize_data)
+    
+    write_file(unnormalize_data, folder_to_write + "/unnormalized")
+    
+    return None
 
 if __name__ == '__main__':
     main()
