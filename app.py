a1ea89184d279e7756481ce72afca8c006997648
Daniel Glez de Chaves González <alu0101407951@ull.edu.es>
2024-03-02 20:55:40 +0000

    algo raro dokcer

--- app.py
+++ app.py
@@ -6,7 +6,8 @@ import os
 from matplotlib import pyplot as plt
 import numpy as np
 import pandas as pd
-import redis
+from sqlalchemy import create_engine
+import seaborn as sns
 
 def read_folder_file(folder_or_file_data: str, headers:bool, separator) -> Any:
 
@@ -115,36 +116,73 @@ def write_file(data_array: Any, folder_to_write: str, headers: bool, separator:s
             
     return None
 
+def setup_logger(log_to_file: bool, log_level: str) -> None:
+    # Configure the logger based on user preferences.
 
-def save_in_redis_db (data_array: Any, db_name: str, headers: bool, separator:str, folder_to_write: str) -> None:
+    # Args:
+    # log_to_file (bool): If True, logs are written to a file. If False, logs are printed to the console.
+    # log_level (str): Level of logging. Example: 'INFO', 'ERROR'.
+
+    # Returns:
+    # None
     
-    r = redis.Redis(host='localhost', port=6379, db=0)
-    
-    if headers:
-        # Write the data to the folder with the name of the column from the dataframe
-        for i in range(len(data_array)):
-            print ("escribiendo archivo: " + folder_to_write + "/file" + str(i) + ".csv")
-            r.set("file" + str(i), data_array[i].to_csv(index=False, sep=separator))
+    if log_to_file:
+        logger.add("debug.log", format="{time} {level} {message}", level=log_level.upper())
     else:
-        # Write the data to the folder without the name of the column from the dataframe
-        for i in range(len(data_array)):
-            print ("escribiendo archivo: " + folder_to_write + "/file" + str(i) + ".csv")
-            r.set("file" + str(i), data_array[i].to_csv(header=False, index=False, sep=separator))
-            
-    return None
+        logger.remove()
+        logger.add(lambda msg: click.echo(msg, err=True), format="{time} {level} {message}", level=log_level.upper())
+
+def build_db_uri(db_name: str, host: str = None, port: str = None, user: str = None, password: str = None) -> str:
+    # Build a database URI based on the database type and credentials.
+
+    # Args:
+    # db_name (str): Name or path of the database.
+    # host (str, optional): Host of the database (for PostgreSQL).
+    # port (str, optional): Port of the database (for PostgreSQL).
+    # user (str, optional): Username for the database (for PostgreSQL).
+    # password (str, optional): Password for the database (for PostgreSQL).
+
+    # Returns:
+    # str: Database URI.
+
+    return f"postgresql://{user}:{password}@{host}:{port}/{db_name}"
 
 
+def load_csv_to_db(csv_file: str, db_uri: str, table_name: str) -> None:
+    # Load a CSV file into a database using SQLAlchemy.
+
+    # Args:
+    # csv_file (str): Path to the CSV file.
+    # db_uri (str): Database URI.
+    # table_name (str): Name of the table to which the CSV data will be loaded.
+
+    # Returns:
+    # None
+    
+    try:
+        df = pd.read_csv(csv_file)
+
+        engine = create_engine(db_uri)
+        with engine.connect() as conn:
+            df.to_sql(table_name, conn, if_exists='replace', index=False)
+            logger.info(f"CSV data from '{csv_file}' successfully loaded into table '{table_name}' in database.")
+    except Exception as e:
+        logger.error(f"Error loading data: {e}")
+        raise
+    
 @click.command()
 @click.argument('folder_or_file_data', type=str)
 @click.argument('folder_to_write', type=str)
-@click.option('--headers', is_flag=True, help="Input include headers.")
+@click.option('--headers', type=bool, default=True, help="Input include headers.")
 @click.option('--separator', type=str, default=';', help="Separator of the data.")
 @click.option('--jitter', is_flag=True, help="Add jitter to the data.")
 @click.option('--normalize', is_flag=True, help="Normalize the data.")
 @click.option('--permutation', is_flag=True, help="Permute the data.")
 @click.option('--magnitude-warp', is_flag=True, help="Magnitude warp the data.")
+@click.option('--log-to-file', is_flag=False, help="Enable logging to a file instead of the console.")
+@click.option('--log-level', default='INFO', type=str, help="Set the logging level (e.g., INFO, ERROR).")
 
-def main(folder_or_file_data: str, folder_to_write: str, headers: bool, separator :str, jitter: bool, normalize : bool, permutation :bool, magnitude_warp: bool) -> None:
+def main(folder_or_file_data: str, folder_to_write: str, headers: bool, separator :str, jitter: bool, normalize : bool, permutation :bool, magnitude_warp: bool , log_to_file: bool, log_level: str) -> None:
 
     data = read_folder_file(folder_or_file_data, headers, separator)
     
@@ -186,19 +224,37 @@ def main(folder_or_file_data: str, folder_to_write: str, headers: bool, separato
         
     write_file(data_modified, folder_to_write + "/final", headers, separator)
 
-    ## represent the data in a graph
-    i = 0
-    for data in data_modified:
-        
-        print ("graficando archivo: " + folder_to_write + "/final/file" + str(i) + ".csv")
-        plt.plot(data[0], data[1])
-        i+=1
-    plt.show()
-    
-    db_name = "redis"
-    
-    save_in_redis_db(data_modified, db_name, headers, separator, folder_to_write)    
 
+    ## Write the data to a database in postgresql through the docker container
+    
+    setup_logger(log_to_file, log_level)
+
+    db_uri = build_db_uri(folder_to_write, "localhost", "5432", "postgres", "postgres")
+
+    try:
+        
+        ## Obtain the name of all the files in the folder to load them up to the database
+        for filename in os.listdir(folder_to_write + "/final"):
+            csv_file = os.path.join(folder_to_write + "/final", filename)
+            table_name = filename.split(".")[0]
+            print(f"Loading data from '{csv_file}' into table '{table_name}' in database.")
+            load_csv_to_db(csv_file, db_uri, table_name)
+            
+    except Exception as e:
+        click.echo(e)
+    ## represent the data in a graph
+    # i = 0
+    # for data in data_modified:
+        
+    #     print ("graficando archivo: " + folder_to_write + "/final/file" + str(i) + ".csv")
+    #     plt.plot(data[0], data[1])
+    #     i+=1
+    # plt.show()
+    
+    # Seaborn graph representation
+    for data in data_modified:
+        sns.lineplot(data=data)
+    plt.show()
     
     return None
 
